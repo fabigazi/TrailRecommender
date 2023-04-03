6988e7c86608a22bb04a2eb6390cd266a95b41b2
fabigazi <fgaziano98@gmail.com>
2023-04-03 13:58:28 -0400

    rearanged files, integrated demo.py into the trail_recommend.py

--- src/trail_recommend.py
+++ src/trail_recommend.py
@@ -1,8 +1,14 @@
+from preprocess_data import *
+from filter_dataframe import *
+from calculate_similarity import *
+from sort_df import *
 import customtkinter
 from PIL import Image
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
+
+import os
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -71,6 +77,23 @@ class TrailRec(customtkinter.CTk):
 
     def open_input_dialog_event(self):
         # Add df rows to treeview
+        user_preferences = {"trail-running": (1, .05), "length": (5000, .05), "loop": (1, .9)}
+        user_location = {'lat': 45.316094085851695, 'lng': -69.3804148103108}
+        distance_threshold = 90
+
+        # get trails data path
+        current_directory = os.getcwd()
+        trail_data_path = os.path.abspath(os.path.join(current_directory, os.pardir)) + "/data/trails.csv"
+
+        # if you have issues with the file path (like me) you can just hardcode it
+        # trail_data_path = '/Users/btwitchell/OneDrive - Northeastern University/Classes/5800/final project/TrailRecommender/data/trails.csv'
+
+        # flow of function calls to return dataframe ranked by user preference
+        df = preprocess_data(trail_data_path)
+        df_filtered = filter_dataframe(user_location, distance_threshold, df)
+        df_filtered = calculate_similarity(user_preferences, df_filtered)
+        df_sorted = sort_df(df_filtered)
+
         for index, row in df_sorted.iterrows():
             self.treeview_frame.insert("", tk.END, values=list(row))
 
