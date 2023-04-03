670ab423a59fef61495d6053030f22537e9b6dd4
fabigazi <fgaziano98@gmail.com>
2023-04-03 15:37:25 -0400

    check boxes

--- src/trail_recommend.py
+++ src/trail_recommend.py
@@ -1,3 +1,5 @@
+import math
+
 from preprocess_data import *
 from filter_dataframe import *
 from calculate_similarity import *
@@ -39,11 +41,10 @@ class TrailRec(customtkinter.CTk):
         self.tab_view_filters = customtkinter.CTkTabview(self, width=250, height=100)
         self.tab_view_filters.grid(row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.tab_view_filters.add("Trail Preferences")
-        self.tab_view_filters.add("Tab 2")
-        self.tab_view_filters.add("Tab 3")
-        self.tab_view_filters.tab("Trail Preferences").grid_columnconfigure(3,
-                                                                            weight=1)  # configure grid of individual tabs
-        self.tab_view_filters.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        self.tab_view_filters.add("Features")
+        self.tab_view_filters.tab("Trail Preferences").grid_columnconfigure(3, weight=1)  # configure grid of
+        # individual tabs
+        self.tab_view_filters.tab("Features").grid_columnconfigure(0, weight=1)
 
         # self.tabview.tab("tabX") assigns the obect to the given tabX tab
         self.states_menu = customtkinter.CTkOptionMenu(self.tab_view_filters.tab("Trail Preferences"),
@@ -63,8 +64,16 @@ class TrailRec(customtkinter.CTk):
                                                      command=self.open_input_dialog_event)
         self.search_button.grid(row=2, column=5, padx=20, pady=(10, 10))
 
-        self.label_tab_2 = customtkinter.CTkLabel(self.tab_view_filters.tab("Tab 2"), text="CTkLabel on Tab 2")
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
+        # added to the features tab
+        self.feature_switches = []
+
+        features = ["beach", "cave", "city-walk", "forest", "historic-site", "hot-springs", "lake", "partially-paved",
+                    "paved", "rails-trails", "river", "views", "waterfall", "wild-flowers", "wildlife"]
+        for i in range(len(features)):
+            switch = customtkinter.CTkSwitch(self.tab_view_filters.tab("Features"),
+                                             text=features.__getitem__(i))
+            switch.grid(row=math.floor(i / 5), column=i - math.floor(i / 5) * 5, padx=10, pady=(10, 10))
+            self.feature_switches.append(switch)
 
         # tree view frame
         self.treeview_frame = ttk.Treeview(self, height=10, padding=1, show="headings")
@@ -77,7 +86,8 @@ class TrailRec(customtkinter.CTk):
 
     def open_input_dialog_event(self):
         # Add df rows to treeview
-        user_preferences = {"trail-running": (1, .05), "length": (5000, .05), "loop": (1, .9)}
+        user_preferences = {"trail-running": (1, .05), "length": (5000, .05), "loop": (1, .0)}
+        #user_preferences = {"trail-running": (1, .05), "length": (5000, .05)}
         user_location = {'lat': 45.316094085851695, 'lng': -69.3804148103108}
         distance_threshold = 90
 
@@ -85,8 +95,9 @@ class TrailRec(customtkinter.CTk):
         current_directory = os.getcwd()
         trail_data_path = os.path.abspath(os.path.join(current_directory, os.pardir)) + "/data/trails.csv"
 
-        # if you have issues with the file path (like me) you can just hardcode it
-        # trail_data_path = '/Users/btwitchell/OneDrive - Northeastern University/Classes/5800/final project/TrailRecommender/data/trails.csv'
+        # if you have issues with the file path (like me) you can just hardcode it trail_data_path =
+        # '/Users/btwitchell/OneDrive - Northeastern University/Classes/5800/final
+        # project/TrailRecommender/data/trails.csv'
 
         # flow of function calls to return dataframe ranked by user preference
         df = preprocess_data(trail_data_path)
@@ -96,7 +107,6 @@ class TrailRec(customtkinter.CTk):
 
         for index, row in df_sorted.iterrows():
             self.treeview_frame.insert("", tk.END, values=list(row))
-
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
