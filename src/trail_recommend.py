7898871bb5dc6ab2c0fb5a3ab2bb538183f6e830
fabigazi <fgaziano98@gmail.com>
2023-04-03 22:57:01 -0400

    Mostly hooked up, lat and long diolog prompt, trail_recommend file clean up, project file clean up deleted treeview, main, and complex example

--- src/trail_recommend.py
+++ src/trail_recommend.py
@@ -5,31 +5,36 @@ from filter_dataframe import *
 from calculate_similarity import *
 from sort_df import *
 import customtkinter
-from PIL import Image
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 
 import os
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
+latitude = 45.316094085851695
+longitude = -69.3804148103108
 
-def states():
-    return ["None", "MA", "NH", "MI", "VT", "CT", "RI"]
-
-
-df_sorted = pd.read_csv(r'C:\Users\fgazi\AllTrails.csv')
+df_sorted = df = pd.DataFrame({'name': pd.Series(dtype='str'),
+                               'area_name': pd.Series(dtype='str'),
+                               'city_name': pd.Series(dtype='str'),
+                               'state_name': pd.Series(dtype='str')})
 
 df_sorted = df_sorted[['name', 'area_name', 'city_name', 'state_name']]
 df_sorted = df_sorted.rename(
     columns={'name': 'Trail Name', 'area_name': 'Area', 'city_name': 'City', 'state_name': 'State'})
+features = []
+
+user_preferences = {}
 
 
 class TrailRec(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+
+        self.is_on = {}
 
         # configure window
         self.title("Trail Recommender")
@@ -51,11 +56,6 @@ class TrailRec(customtkinter.CTk):
                                                        placeholder_text="Popularity")
         self.popularity_entry.grid(row=0, column=0, padx=20, pady=(10, 10))
 
-        # self.states_menu = customtkinter.CTkOptionMenu(self.tab_view_filters.tab("Trail Preferences"),
-        #                                              dynamic_resizing=True,
-        #                                             values=states())
-
-        # self.states_menu.grid(row=0, column=0, padx=20, pady=(10, 10))
         self.length_entry = customtkinter.CTkEntry(self.tab_view_filters.tab("Trail Preferences"),
                                                    placeholder_text="length in mi")
         self.length_entry.grid(row=0, column=1, padx=20, pady=(10, 10))
@@ -64,18 +64,15 @@ class TrailRec(customtkinter.CTk):
                                                       placeholder_text="elevation in ft")
         self.elevation_entry.grid(row=0, column=2, padx=20, pady=(10, 10))
 
-        #self.slider_1 = customtkinter.CTkSlider(self.tab_view_filters.tab("Trail Preferences"), from_=0, to=1,
-         #                                       number_of_steps=4)
-        #self.slider_1.grid(row=0, column=3, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.slider_1 = customtkinter.CTkSlider(self.tab_view_filters.tab("Trail Preferences"), from_=0, to=1,
+        #                                       number_of_steps=4)
+        # self.slider_1.grid(row=0, column=3, padx=(20, 10), pady=(10, 10), sticky="ew")
 
         self.difficulty = customtkinter.CTkOptionMenu(self.tab_view_filters.tab("Trail Preferences"),
-                                                       dynamic_resizing=True, values=["Any", "1", "2", "3", "4", "5"])
+                                                      dynamic_resizing=True,
+                                                      values=["Any Difficulty", "1", "2", "3", "4", "5"])
 
         self.difficulty.grid(row=0, column=3, padx=20, pady=(10, 10))
-
-        self.switch_dog_friendly = customtkinter.CTkSwitch(self.tab_view_filters.tab("Trail Preferences"),
-                                                           text=f"Dog Friendly")
-        self.switch_dog_friendly.grid(row=2, column=0, padx=20, pady=(10, 10))
 
         activity = ["Any Activity", "backpacking", "bike-touring", "birding", "camping", "canoeing",
                     "cross-country-skiing", "fishing", "fly-fishing", "hiking", "horseback-riding", "ice-climbing",
@@ -84,28 +81,37 @@ class TrailRec(customtkinter.CTk):
                     "trail-running", "walking", "whitewater-kayaking"]
 
         self.activity = customtkinter.CTkOptionMenu(self.tab_view_filters.tab("Trail Preferences"),
-                                                      dynamic_resizing=False, values=activity)
+                                                    dynamic_resizing=False, values=activity)
 
         self.activity.grid(row=0, column=4, padx=20, pady=(10, 10))
 
+        self.switch_dog_friendly = customtkinter.CTkSwitch(self.tab_view_filters.tab("Trail Preferences"),
+                                                           text=f"Dog Friendly")
+        self.switch_dog_friendly.grid(row=2, column=0, padx=20, pady=(10, 10))
+
+        self.lat_long = customtkinter.CTkButton(self.tab_view_filters.tab("Trail Preferences"), text="Lat & Long",
+                                                command=self.input_latitude_and_longitude)
+        self.lat_long.grid(row=0, column=5, padx=20, pady=(10, 10))
 
         self.search_button = customtkinter.CTkButton(self.tab_view_filters.tab("Trail Preferences"), text="Search",
-                                                     command=self.open_input_dialog_event)
+                                                     command=self.run_search)
         self.search_button.grid(row=2, column=5, padx=20, pady=(10, 10))
 
         # added to the features tab
         self.feature_switches = []
 
+        global features
         features = ["beach", "cave", "city-walk", "forest", "historic-site", "hot-springs", "lake", "partially-paved",
                     "paved", "rails-trails", "river", "views", "waterfall", "wild-flowers", "wildlife"]
         for i in range(len(features)):
             switch = customtkinter.CTkSwitch(self.tab_view_filters.tab("Features"),
                                              text=features.__getitem__(i))
             switch.grid(row=math.floor(i / 8), column=i - math.floor(i / 8) * 8, padx=10, pady=(10, 10))
+            self.is_on[features[i]] = False
             self.feature_switches.append(switch)
 
         # tree view frame
-        self.treeview_frame = ttk.Treeview(self, height=10, padding=1, show="headings")
+        self.treeview_frame = ttk.Treeview(self, height=20, padding=1, show="headings")
         self.treeview_frame.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.treeview_frame['columns'] = list(df_sorted.columns)
 
@@ -113,51 +119,67 @@ class TrailRec(customtkinter.CTk):
         for column in df_sorted.columns:
             self.treeview_frame.heading(column, text=column)
 
-    def open_input_dialog_event(self):
+    def run_search(self):
         # Add df rows to treeview
-
-        prefence_dict = {}
-
-        array = [('distance', 500), ('hiking', 1), ("running", 0)]
-
-        for i in array:
-            if i[1] > 0:
-                prefence_dict[i[0]] = i[1]
-
-        print(prefence_dict)
+        global user_preferences
         user_preferences = {"trail-running": (1, .05), "length": (5000, .05), "loop": (1, .0)}
-        # user_preferences = {"trail-running": (1, .05), "length": (5000, .05)}
-        user_location = {'lat': 45.316094085851695, 'lng': -69.3804148103108}
-        distance_threshold = 90
+
+        for i in range(len(features)):
+            if self.feature_switches[i].__getattribute__("_check_state"):
+                user_preferences[features[i]] = (1, .05)
+
+        if not (self.popularity_entry.get() == ""):
+            user_preferences["popularity"] = (int(self.popularity_entry.get()), .05)
+
+        if not (self.length_entry.get() == ""):
+            user_preferences["length"] = (int(self.length_entry.get()), .05)
+
+        if not (self.elevation_entry.get() == ""):
+            user_preferences["elevation_gain"] = (int(self.elevation_entry.get()), .05)
+
+        if not (self.elevation_entry.get() == ""):
+            user_preferences["elevation_gain"] = (int(self.elevation_entry.get()), .05)
+
+        if not (self.difficulty.get() == "Any Difficulty"):
+            user_preferences["difficulty_rating"] = (int(self.difficulty.get()), .05)
+
+        if not (self.activity.get() == "Any Activity"):
+            user_preferences[self.activity.get()] = (1, .05)
+
+        user_location = {'lat': latitude, 'lng': longitude}
+        distance_threshold = 1000
 
         # get trails data path
         current_directory = os.getcwd()
         trail_data_path = os.path.abspath(os.path.join(current_directory, os.pardir)) + "/data/trails.csv"
 
-        # if you have issues with the file path (like me) you can just hardcode it trail_data_path =
-        # '/Users/btwitchell/OneDrive - Northeastern University/Classes/5800/final
-        # project/TrailRecommender/data/trails.csv'
-
         # flow of function calls to return dataframe ranked by user preference
-        df = preprocess_data(trail_data_path)
-        df_filtered = filter_dataframe(user_location, distance_threshold, df)
+        df1 = preprocess_data(trail_data_path)
+        df_filtered = filter_dataframe(user_location, distance_threshold, df1)
         df_filtered = calculate_similarity(user_preferences, df_filtered)
+        global df_sorted
         df_sorted = sort_df(df_filtered)
+        # print(df_sorted)
 
-        for index, row in df_sorted.iterrows():
+        reduced_df = df_sorted[["name", "area_name", "city_name", "state_name"]]
+
+        for index, row in reduced_df.iterrows():
             self.treeview_frame.insert("", tk.END, values=list(row))
 
-    def change_appearance_mode_event(self, new_appearance_mode: str):
-        customtkinter.set_appearance_mode(new_appearance_mode)
-
-    def change_scaling_event(self, new_scaling: str):
-        new_scaling_float = int(new_scaling.replace("%", "")) / 100
-        customtkinter.set_widget_scaling(new_scaling_float)
-
-    def sidebar_button_event(self):
-        print("sidebar_button click")
+    def input_latitude_and_longitude(self):
+        dialog = customtkinter.CTkInputDialog(text="Latitude, Longitude in degrees separated by a comma",
+                                              title="Latitude")
+        try:
+            split = dialog.get_input().split(",")
+            global latitude
+            global longitude
+            latitude = float(split[0])
+            longitude = float(split[1])
+        except ValueError:
+            self.input_latitude_and_longitude()
 
 
 if __name__ == "__main__":
     app = TrailRec()
+    app.input_latitude_and_longitude()
     app.mainloop()
